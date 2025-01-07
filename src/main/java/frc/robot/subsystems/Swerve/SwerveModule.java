31b2fb0db6702cd6691238d9443a90db71161d62
Joe Pauly <joeypauly33@gmail.com>
2025-01-07 17:44:33 -0600

    Added the getAngle method to give us the angle of a module in degrees

--- src/main/java/frc/robot/subsystems/Swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveModule.java
@@ -16,48 +16,59 @@ import com.revrobotics.spark.SparkMax;
 
 public class SwerveModule {
 
-    private SparkMax DriveMotor;
-    private SparkMax AngleMotor;
-    private RelativeEncoder DriveEncoder;
-    private CANcoder AngleEncoder;
-    private CANcoderConfiguration AngleEncoderConfiguration;
-    private double AngleEncoderOffset;
-    private SparkMaxConfig DriveMotorConfig;
+    private SparkMax driveMotor;
+    private SparkMax angleMotor;
+    private RelativeEncoder driveEncoder;
+    private CANcoder angleEncoder;
+    private CANcoderConfiguration angleEncoderConfiguration;
+    private double angleEncoderOffset;
+    private SparkMaxConfig driveMotorConfig;
+    private double angleAsDouble;
+    private double moduleAngle;
 
     //Constructor that allows for all of the modules to be created in the subsytem by feeding in the ids and offsets
     public SwerveModule(int driveMotorID, int angleMotorID, int CANCoderID, double AngleEncoderOffset){
 
         //Defines the motor in the constructor to tell the rest of the clas it exists
-        DriveMotor = new SparkMax(driveMotorID, MotorType.kBrushless);
+        driveMotor = new SparkMax(driveMotorID, MotorType.kBrushless);
 
         //Defines the drive encoder of off the drive motor to tell us how many times the robot has spun
-        DriveEncoder = DriveMotor.getEncoder();
+        driveEncoder = driveMotor.getEncoder();
 
         //Defines the motor and the CANCoder
-        AngleMotor = new SparkMax(angleMotorID, MotorType.kBrushless);
-        AngleEncoder = new CANcoder(CANCoderID);
+        angleMotor = new SparkMax(angleMotorID, MotorType.kBrushless);
+        angleEncoder = new CANcoder(CANCoderID);
 
         //Makes a configurator object for the CANCoder allowing us to change specific parts of it
-        AngleEncoderConfiguration = new CANcoderConfiguration();
+        angleEncoderConfiguration = new CANcoderConfiguration();
 
         //Changes the magnet sensor settings to work better with our robot
-        AngleEncoderConfiguration.MagnetSensor.withAbsoluteSensorDiscontinuityPoint(Constants.ABSOLUTE_ENCODER_DISCONTINUITY_POINT);
-        AngleEncoderConfiguration.MagnetSensor.MagnetOffset = AngleEncoderOffset;
-        AngleEncoderConfiguration.MagnetSensor.SensorDirection = Constants.ABSOLUTE_ENCODER_SENSOR_DIRECTION;
+        angleEncoderConfiguration.MagnetSensor.withAbsoluteSensorDiscontinuityPoint(Constants.ABSOLUTE_ENCODER_DISCONTINUITY_POINT);
+        angleEncoderConfiguration.MagnetSensor.MagnetOffset = AngleEncoderOffset;
+        angleEncoderConfiguration.MagnetSensor.SensorDirection = Constants.ABSOLUTE_ENCODER_SENSOR_DIRECTION;
 
         //applies the changes that were made to the CANCoder
-        AngleEncoder.getConfigurator().apply(AngleEncoderConfiguration);
+        angleEncoder.getConfigurator().apply(angleEncoderConfiguration);
+        
+    }
 
+    //Returns the module angle in degrees
+    public Rotation2d getAngle(){
+        //Gets the CTRE value from -0.5 to 0.5
+        double angleAsDouble = angleEncoder.getAbsolutePosition().getValueAsDouble();
+        //Multiplies the value of -0.5 to 0.5 giving us the value as an angle
+        double moduleAngle = 360 * angleAsDouble;
+        return new Rotation2d(moduleAngle);
     }
 
     //Gets the state of this module by giving you the wheel veloicty and the value of the wheel angle
     public SwerveModuleState getState(){
-        return new SwerveModuleState(DriveEncoder.getVelocity(), new Rotation2d(AngleEncoder.getAbsolutePosition().getValue()));
+        return new SwerveModuleState(driveEncoder.getVelocity(), new Rotation2d(angleEncoder.getAbsolutePosition().getValue()));
     }
 
     //Gets the current position of the robot or where it is
     public SwerveModulePosition getPosition(){
-        return new SwerveModulePosition(DriveEncoder.getPosition(), new Rotation2d(AngleEncoder.getAbsolutePosition().getValue()));
+        return new SwerveModulePosition(driveEncoder.getPosition(), new Rotation2d(angleEncoder.getAbsolutePosition().getValue()));
     }
 
     //Sets the desired wheel state of this module for the robot
